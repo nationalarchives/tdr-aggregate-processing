429b64771af7f81f27878a324531aa41772777ee
TomJKing <tom.king@nationalarchives.gov.uk>
2025-07-29 12:27:54 +0100

    List all S3 objects for a given object prefix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,24 @@
 import sbt.*
 
 object Dependencies {
+  private val circeVersion = "0.14.14"
+
+  lazy val awsLambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.3.0"
+  lazy val awsLambdaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.16.1"
+  lazy val awsSqs = "com.amazonaws" % "aws-java-sdk-sqs" % "1.12.788"
+
+  lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
+  lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
+  lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
+
+  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.5.18"
+  lazy val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "8.1"
+
+  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.286"
+  lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
+
+  lazy val typesafeConfig = "com.typesafe" % "config" % "1.4.4"
+
+  lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.1"
 }
--- src/main/scala/uk/gov/nationalarchives/aggregate/processing/AggregateProcessingLambda.scala
+++ src/main/scala/uk/gov/nationalarchives/aggregate/processing/AggregateProcessingLambda.scala
@@ -1,7 +1,49 @@
 package uk.gov.nationalarchives.aggregate.processing
 
-class AggregateProcessingLambda {
-  def processDataLoad(): String = {
-    "Hello world"
+import com.amazonaws.services.lambda.runtime.events.SQSEvent
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import io.circe._
+import io.circe.generic.semiauto.deriveDecoder
+import io.circe.parser._
+import com.typesafe.scalalogging.Logger
+import uk.gov.nationalarchives.aggregate.processing.AggregateProcessingLambda.{AggregateEvent, logger, s3Utils}
+import uk.gov.nationalarchives.aws.utils.s3.{S3Clients, S3Utils}
+import com.typesafe.config.{Config, ConfigFactory}
+import software.amazon.awssdk.services.s3.model.S3Object
+
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+
+class AggregateProcessingLambda extends RequestHandler[SQSEvent, Unit] {
+  implicit val sharepointInputDecoder: Decoder[AggregateEvent] = deriveDecoder[AggregateEvent]
+
+  override def handleRequest(event: SQSEvent, context: Context): Unit = {
+    val sqsMessages: Seq[SQSMessage] = event.getRecords.asScala.toList
+    sqsMessages.foreach(m => process(m))
   }
+
+  def process(sqsMessage: SQSMessage): List[S3Object] = {
+    val event = parseSqsMessage(sqsMessage)
+    logger.info(s"Processing assets with prefix ${event.metadataSourceObjectPrefix}")
+    s3Utils.listAllObjectsWithPrefix(event.metadataSourceBucket, event.metadataSourceObjectPrefix)
+  }
+
+  private def parseSqsMessage(sqsMessage: SQSMessage): AggregateEvent = {
+    parse(sqsMessage.getBody) match {
+      case Left(parsingError) =>
+        throw new IllegalArgumentException(s"Invalid JSON object: ${parsingError.message}")
+      case Right(json) => decode[AggregateEvent](json.toString()) match {
+        case Left(decodingError) => throw new IllegalArgumentException(s"Invalid event: ${decodingError.getMessage}")
+        case Right(event) => event
+      }
+    }
+  }
+}
+
+object AggregateProcessingLambda {
+  val logger: Logger = Logger[AggregateProcessingLambda]
+  private val configFactory: Config = ConfigFactory.load()
+  val s3Utils: S3Utils = S3Utils(S3Clients.s3Async(configFactory.getString("s3.endpoint")))
+
+  case class AggregateEvent(metadataSourceBucket: String, metadataSourceObjectPrefix: String)
 }
