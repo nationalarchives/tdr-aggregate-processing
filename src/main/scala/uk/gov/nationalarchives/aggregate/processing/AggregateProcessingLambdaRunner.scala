5cf659c2fa4e634ef031847c9442044b144289b1
TomJKing <tom.king@nationalarchives.gov.uk>
2025-09-17 08:13:36 +0100

    Revert lambda runner

--- src/main/scala/uk/gov/nationalarchives/aggregate/processing/AggregateProcessingLambdaRunner.scala
+++ src/main/scala/uk/gov/nationalarchives/aggregate/processing/AggregateProcessingLambdaRunner.scala
@@ -4,7 +4,7 @@ import cats.effect.unsafe.implicits.global
 import uk.gov.nationalarchives.aggregate.processing.AggregateProcessingLambda.AggregateEvent
 
 object AggregateProcessingLambdaRunner extends App {
-  val event = AggregateEvent("tdr-upload-files-cloudfront-dirty-staging", "e5c347a6-ca8b-433f-9d50-c40809a0cfd9/sharepoint/bde99d64-e39d-402c-a0f8-f3a61b0d5f99/metadata")
+  val event = AggregateEvent("placeholder", "placeholder")
 
   new AggregateProcessingLambda().processEvent(event).unsafeRunSync()
 }
