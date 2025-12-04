fbe7441dfdac903ad53b270ffc12811b6e0c1b9b
TomJKing <tom.king@nationalarchives.gov.uk>
2025-12-04 09:18:19 +0000

    Add malware scan tags to reduce cost

--- src/test/scala/uk/gov/nationalarchives/aggregate/processing/StressTestSetup.scala
+++ src/test/scala/uk/gov/nationalarchives/aggregate/processing/StressTestSetup.scala
@@ -1,7 +1,7 @@
 package uk.gov.nationalarchives.aggregate.processing
 
 import software.amazon.awssdk.core.sync.RequestBody
-import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import software.amazon.awssdk.services.s3.model.{PutObjectRequest, Tag, Tagging}
 import uk.gov.nationalarchives.aws.utils.s3.S3Clients.s3
 
 import java.io.ByteArrayInputStream
@@ -13,12 +13,14 @@ object StressTestSetup extends App {
 
   /*
    * Instructions
-   * 1. In the TDR application start a transfer in the desired environment (intg / staging), noting down the consignment id
-   * 2. Replace the 'placeholder' values with the correct values for the transfer
-   * 3. Set the 'numberOfAssets' to the desired number of files to process
-   * 4. Add AWS credentials giving permission to put objects into the relevant S3 bucket
-   * 5. Run the app to upload the generated records / metadata to the S3 bucket
-   * 6. Send SQS message to trigger the lambda to process the consignment
+   * 1. Temporarily disable the AWS GuardDuty malware scan on the S3 bucket: AWS Console -> GuardDuty -> Malware Protection -> S3
+   * 2. In the TDR application start a transfer in the desired environment (intg / staging), noting down the consignment id
+   * 3. Replace the 'placeholder' values with the correct values for the transfer
+   * 4. Set the 'numberOfAssets' to the desired number of files to process
+   * 5. Add AWS credentials giving permission to put objects into the relevant S3 bucket
+   * 6. Run the app to upload the generated records / metadata to the S3 bucket
+   * 7. Send SQS message to trigger the lambda to process the consignment
+   * 8. Enable AWS GuardDuty malware scan on the S3 bucket: AWS Console -> GuardDuty -> Malware Protection -> S3
    * */
 
   val s3Client = s3("https://s3.eu-west-2.amazonaws.com/")
@@ -26,6 +28,12 @@ object StressTestSetup extends App {
   val numberOfAssets = 0
   val userId = "placeholder"
   val consignmentId = "placeholder"
+  val malwareScanTag = Tagging.builder()
+    .tagSet(Tag.builder()
+      .key("GuardDutyMalwareScanStatus")
+      .value("NO_THREATS_FOUND")
+      .build())
+    .build()
 
   (1 to numberOfAssets).foreach { i =>
     val matchId = UUID.randomUUID()
@@ -38,7 +46,10 @@ object StressTestSetup extends App {
     val fileContent = inputStream.readAllBytes().map(_.toChar).mkString
     val fileSize = fileContent.getBytes().length
     val fileBody = RequestBody.fromString(fileContent)
-    val fileRequest = PutObjectRequest.builder.bucket(s3OutputBucket).key(s"$userId/sharepoint/$consignmentId/records/$matchId").build
+    val fileRequest = PutObjectRequest.builder
+      .bucket(s3OutputBucket).key(s"$userId/sharepoint/$consignmentId/records/$matchId")
+      .tagging(malwareScanTag)
+      .build
     s3Client.putObject(fileRequest, fileBody)
 
     val metadataJsonString =
@@ -55,7 +66,10 @@ object StressTestSetup extends App {
          |""".stripMargin
 
     val metadataBody = RequestBody.fromString(metadataJsonString)
-    val metadataRequest = PutObjectRequest.builder.bucket(s3OutputBucket).key(s"$userId/sharepoint/$consignmentId/metadata/$matchId.metadata").build
+    val metadataRequest = PutObjectRequest.builder
+      .bucket(s3OutputBucket).key(s"$userId/sharepoint/$consignmentId/metadata/$matchId.metadata")
+      .tagging(malwareScanTag)
+      .build
     s3Client.putObject(metadataRequest, metadataBody)
   }
 
