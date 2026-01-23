1e3cd4695cdf3dbedf1d29354499d08818f8c38e
TomJKing <tom.king@nationalarchives.gov.uk>
2026-01-23 13:42:44 +0000

    Use aws S3 utils function to put errors

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
   private val circeVersion = "0.14.15"
   private val mockitoScalaVersion = "2.0.0"
-  private val awsUtilsVersion = "0.1.315"
+  private val awsUtilsVersion = "0.1.316"
 
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.267"
   lazy val awsLambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.4.0"
