0791cf5caebde8e4872f70f77e8320fe6a96b1c3
Thanh Lam <thanh.lam@nationalarchives.gov.uk>
2026-02-02 11:46:06 +0000

    Initial commit

--- src/main/scala/uk/gov/nationalarchives/aggregate/processing/AggregateProcessingLambdaRunner.scala
+++ src/main/scala/uk/gov/nationalarchives/aggregate/processing/AggregateProcessingLambdaRunner.scala
@@ -4,7 +4,7 @@ import cats.effect.unsafe.implicits.global
 import uk.gov.nationalarchives.aggregate.processing.AggregateProcessingLambda.AggregateEvent
 
 object AggregateProcessingLambdaRunner extends App {
-  val event = AggregateEvent("metadataSourceBucket", "metadataSourceObjectPrefix", dataLoadErrors = false)
+  val event = AggregateEvent("tdr-upload-files-cloudfront-dirty-intg", "54a5e6d5-37eb-4ed5-b159-6ec1a899530b/sharepoint/73f4899c-aac9-4235-bfc9-818ea1c7d1c7/metadata", dataLoadErrors = false)
 
   new AggregateProcessingLambda().processEvent(event).unsafeRunSync()
 }
